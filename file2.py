d0d9eea64278b523f647d86c89bc2ced17e94eff
Paul Glezen <pglezen@isab.lacounty.gov>
2020-10-12 22:15:31 -0700

    Added Python and fixed typos.

---
+++ file2.py
@@ -0,0 +1,9 @@
+def print_usage():
+   usage = """Usage: addAudit.py [-f] [-v] <filename ...>
+    -f - overwrite when duplicate key encountered
+    -v - verbose
+    <filename ..> the name of at least one audit file."""
+
+   print(usage)
+
+print_usage()
